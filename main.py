297ad7f57cd1ab0c09bcd126aaff9d258763723c
adherwp <adheadhe71@gmail.com>
2025-11-18 12:56:21 +0700

    fix hosting 2

--- main.py
+++ main.py
@@ -184,9 +184,7 @@ def tugas_fungsi():
 
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+if __name__ == '__main__':
+    app.run()
 
 
